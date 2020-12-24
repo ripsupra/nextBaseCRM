f9f807b6c88b7ede121b716599a1a3c8a6fcdefd
Adil Imanaliev <imanaliev.adil@gmail.com>
2020-12-24 11:27:56 -0500

    Intro Class

---
+++ src/test/java/com/nextBaseCRM/tests/IntroClass.java
@@ -0,0 +1,9 @@
+package com.nextBaseCRM.tests;
+
+public class IntroClass {
+    public static void main(String[] args) {
+
+
+        System.out.println("This is going to be my INTO Class to push project into GitHub");
+    }
+}
